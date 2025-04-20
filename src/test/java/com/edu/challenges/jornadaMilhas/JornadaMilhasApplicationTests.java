dca0d14482e5de5ce71cc289e739589c9e7b158e
Bruno Maciel <maciel2bruno@gmail.com>
2025-04-20 20:07:50 -0300

    First Commit

---
+++ src/test/java/com/edu/challenges/jornadaMilhas/JornadaMilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.challenges.jornadaMilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornadaMilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
